cf4e3240edbdc04130565787fd30a158a2580530
nathanbegbie <nathanbegbie@gmail.com>
2020-03-27 12:10:31 +0000

    fix test atom

--- test/peach_test.exs
+++ test/peach_test.exs
@@ -7,34 +7,34 @@ defmodule PeachTest do
   end
 
   test "normalise_text" do
-    assert Peach.normalise_text() == :todo
+    assert Peach.normalise_text() == :to_do
   end
 
   test "remove_emojis" do
-    assert Peach.remove_emojis() == :todo
+    assert Peach.remove_emojis() == :to_do
   end
 
   test "normalise_whitespace" do
-    assert Peach.normalise_whitespace() == :todo
+    assert Peach.normalise_whitespace() == :to_do
   end
 
   test "remove_punc" do
-    assert Peach.remove_punc() == :todo
+    assert Peach.remove_punc() == :to_do
   end
 
   test "replace_punc" do
-    assert Peach.replace_punc() == :todo
+    assert Peach.replace_punc() == :to_do
   end
 
   test "remove_numbers" do
-    assert Peach.remove_numbers() == :todo
+    assert Peach.remove_numbers() == :to_do
   end
 
   test "pre_process" do
-    assert Peach.pre_process() == :todo
+    assert Peach.pre_process() == :to_do
   end
 
   test "get_brief" do
-    assert Peach.get_brief() == :todo
+    assert Peach.get_brief() == :to_do
   end
 end
