40f92456ef0dc1a96d772e5efc8c7d33a5145fe1
Simon de Haan <simon@praekelt.org>
2020-05-14 13:47:47 +0200

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Peach.MixProject do
   def project do
     [
       app: :peach,
-      version: "0.0.1",
+      version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       description: description(),
